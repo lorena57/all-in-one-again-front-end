01406647cf543236ccbd4686ce64323a984d158e
github username <github email address>
2020-11-29 21:28:21 -0800

    add post, and it works

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -4,22 +4,49 @@ class NoteForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            title: '',
+            content: ''
         }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content
+        };
+
+        fetch('http://localhost:3000/api/v1/notes', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(newNote)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
     }
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div>
                         <label>Title:</label>
                         <br/>
-                        <input type="text" name='title'/>
+                        <input type="text" name='title' onChange={this.onChange} value={this.state.title}/>
                     </div>
+                    <br/>
                     <div>
                         <label>Notes:</label><br />
-                        <textarea cols="30" rows="10" name='content'></textarea>
+                        <textarea cols="30" rows="10" name='content' onChange={this.onChange} value={this.state.content}></textarea>
                     </div>
                     <br/>
                     <button type='submit'>Submit</button>
