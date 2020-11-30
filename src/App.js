e78ba553bd490a97803710fa4c8bcd873a6ba51e
github username <github email address>
2020-11-29 21:57:11 -0800

    add provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Notes from "./components/Notes";
 import NoteForm from "./components/NoteForm";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <NoteForm/>
-      <hr/>
-      <Notes/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <NoteForm/>
+        <hr/>
+        <Notes/>
+      </div>
+    </Provider>
   );
 }
 
