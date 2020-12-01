33e60cef0906b3adb84b55d99a6fc96db913ee25
github username <github email address>
2020-11-30 19:55:19 -0800

    add contactReducer to combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import noteReducer from './noteReducer';
+import contactReducer from './contactReducer';
 
 export default combineReducers({
     notes: noteReducer,
-   
+    contacts: contactReducer
 });
