b647f386ada12c45cd5d112581bdaefff9d99d37
github username <github email address>
2020-11-30 18:41:59 -0800

    fetch contact info

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 
  class Contacts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: []
+        }
+    }
+
+    componentWillMount() {
+        fetch('http://localhost:3000/api/v1/contacts')
+        .then(res => res.json())
+        .then(data => this.setState({contacts: data}));
+    }
+
     render() {
+        const contactItems = this.state.contacts.map(contact => (
+            <div key={contact.id}>
+                <h3>First Name: {contact.first_name}</h3>
+                <h3>Last Name: {contact.last_name}</h3>
+                <h3>Email Address: {contact.email_address}</h3>
+                <h3>Contact Number: {contact.phone_number}</h3>
+            </div>
+        ))
         return (
             <div>
                 <h1>Contact</h1>
+                {contactItems}
             </div>
         )
     }
