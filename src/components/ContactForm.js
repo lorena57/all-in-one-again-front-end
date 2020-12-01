5a8508edc2075c56d6e74dda92cff82968ef370c
github username <github email address>
2020-11-30 19:24:23 -0800

    add onChange to form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,9 +4,20 @@ import React, { Component } from 'react'
     constructor(props) {
         super(props)
         this.state = {
-
+            first_name: '',
+            last_name: '',
+            email: '',
+            number: ''
         };
+
+        this.onChange = this.onChange.bind(this);
     }
+
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+
 
     render() {
         return (
@@ -15,20 +26,26 @@ import React, { Component } from 'react'
                 <form>
                     <div>
                         <label>First Name:</label><br/>
-                        <input type="text" />
+                        <input type="text" onChange={this.onChange} name='first_name' value={this.state.first_name} />
                     </div>
+                    <br />
                     <div>
                         <label>Last Name:</label><br />
-                        <input type="text" />
+                        <input type="text" onChange={this.onChange} name='last_name' value={this.state.last_name} />
+                        
                     </div>
+                    <br />
                     <div>
                         <label>Email Address:</label><br />
-                        <input type="text" />
+                        <input type="text" onChange={this.onChange} name='email' value={this.state.email} />
                     </div>
+                    <br />
                     <div>
                         <label>Contact Number:</label><br />
-                        <input type="text" />
+                        <input type="text" onChange={this.onChange} name='number' value={this.state.number} />
+                        <br />
                     </div>
+                    <br />
                     <div>
                         <button type='submit'>Add Contact</button>
                     </div>
