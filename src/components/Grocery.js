f4c43ab848b6a96693b5e924504ffe0949e9488f
github username <github email address>
2020-12-04 19:47:34 -0800

    create grocery page

---
+++ src/components/Grocery.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const Grocery = (props) => {
+    // console.log(props.groceries)
+    let grocery = props.groceries[props.match.params.id - 1]
+    console.log(grocery)
+    return (
+        <div>
+            <h1>Grocery</h1>
+            <li>
+                {grocery ? grocery.item : null}
+            </li>
+        </div>
+    );
+};
+export default Grocery;
