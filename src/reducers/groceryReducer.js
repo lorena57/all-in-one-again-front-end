8848af3a4f4694556400ce1fc60fe2cac96e47e1
github username <github email address>
2020-12-04 17:26:17 -0800

    add and remove groceryReducer code

--- src/reducers/groceryReducer.js
+++ src/reducers/groceryReducer.js
@@ -2,17 +2,20 @@ import { FETCH_GROCERIES } from "../actions/types";
 import _ from 'lodash';
 
 
-// const initialState = {
-//     groceries: [],
-//     grocery: {}
-// }
+const initialState = {
+    groceries: [],
+    grocery: {}
+}
 
-const groceryReducer = (state = {}, action) => {
+const groceryReducer = (state = initialState, action) => {
     switch (action.type) {
-    case FETCH_GROCERIES:
-        return _.mapKeys(action.payload.data, 'id');
-    default: 
-    return state;        
+        case FETCH_GROCERIES:
+            return {
+                ...state,
+                groceries: action.payload
+            }
+        default:
+            return state;
     }
 }
 
