c35bb6c2a91f9737f5c15fc5dc240a7b93ca680a
github username <github email address>
2020-12-04 17:38:44 -0800

    add fix fetch and add delete function, hope it works

--- src/actions/groceryActions.js
+++ src/actions/groceryActions.js
@@ -1,10 +1,20 @@
-import { FETCH_GROCERIES } from './types';
-
+import { FETCH_GROCERIES, DELETE_GROCERY } from './types';
 export const fetchGroceries = () => dispatch => {
     fetch('http://localhost:3000/api/v1/groceries')
         .then(res => res.json())
         .then(groceries => dispatch({
             type: FETCH_GROCERIES,
             payload: groceries
+        }));
+}
+export const deleteGrocery = (groceryId) => dispatch => {
+    debugger
+    fetch(`http://localhost:3000/api/v1/groceries/${groceryId}`, {
+        method: 'DELETE'
+    })
+        .then(res => res.json())
+        .then(groceries => dispatch({
+            type: DELETE_GROCERY,
+            payload: groceryId
         }));
 }
